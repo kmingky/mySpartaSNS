0e32abfc73fa8cd6d7869e2e8e2ae5e2a5e6e19c
DESKTOP-GBFAR6S\KM <minki.kim@kakao.com>
2022-05-29 19:24:46 +0900

    first commit

---
+++ user/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+#user/models.py
+
+# Create your models here.
+
+# Create your models here.
+class UserModel(AbstractUser):
+    class Meta:
+        db_table = "my_user"
+
+    bio = models.CharField(max_length=256, default='')
+    follow = models.ManyToManyField(settings.AUTH_USER_MODEL,related_name='followee')
