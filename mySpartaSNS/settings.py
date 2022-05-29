82b736722294c029e2d8b113714425877acab475
DESKTOP-GBFAR6S\KM <minki.kim@kakao.com>
2022-05-29 19:32:22 +0900

    fix setting

--- mySpartaSNS/settings.py
+++ mySpartaSNS/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a4_nj@=ab$a=h-m8e7vq8&#y8@v%$n4urnkk5=8^jt#h2bid8&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
